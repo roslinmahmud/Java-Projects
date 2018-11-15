75a0ffe3e9f9ae406645605781b4a6819424995f
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2018-11-16 00:49:16 +0600

    Second GUI Project

--- IPAddressFinder.java
+++ IPAddressFinder.java
@@ -1,11 +1,52 @@
 import java.awt.event.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.swing.*;
 
 public class IPAddressFinder implements ActionListener{
     JLabel prompt, ipAddress;
     JFrame f;
-    JTextField tf;
+    JTextField tf, tfIP;
+    JButton b;
+    JLabel l;
     public IPAddressFinder(){
-        
+        f = new JFrame("IP Address Finder");
+
+        l = new JLabel("Enter the URL of the Host");
+        l.setBounds(100, 50, 300, 30);
+
+        tf = new JTextField();
+        tf.setBounds(50, 80, 300, 40);
+
+        tfIP = new JTextField();
+        tfIP.setBounds(20, 130, 360, 50);
+        tfIP.setEditable(false);
+
+        b = new JButton("Find IP");
+        b.setBounds(150, 200, 100, 50);
+        b.addActionListener(this);
+
+        f.add(l);
+        f.add(tf);
+        f.add(tfIP);
+        f.add(b);
+
+        f.setSize(400, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+
+    }
+    public void actionPerformed(ActionEvent e){
+        String host = tf.getText();
+        try{
+            String ip = InetAddress.getByName(host).getHostAddress();
+            tfIP.setText("IP Address of " + host + " is: " + ip);
+        }catch(UnknownHostException uh){
+            tfIP.setText(uh.getMessage());
+        }
+    }
+    public static void main(String args[]){
+        new IPAddressFinder();
     }
 }
